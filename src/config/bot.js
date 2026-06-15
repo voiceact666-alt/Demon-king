cd63274895cdc480c0927e20810f4b086a0430fa
voiceact666-alt <voice.act666@gmail.com>
2026-06-15 15:53:49 +0930

    Update bot.js

--- src/config/bot.js
+++ src/config/bot.js
@@ -25,9 +25,9 @@ export const botConfig = {
     activities: [
       {
         // Text users will see (example: "Playing /help | Titan Bot").
-        name: "Made with ❤️",
+        name: "Made by demon_hunter_va",
         // Activity type number (0 = Playing).
-        type: 0, 
+        type: 4, 
       },
     ],
   },
